ea78bd8091904aae159956e2043f6f7110592b5d
Carlos Jiménez Martín <carlos.jimenez.martin@alumnos.upm.es>
2020-10-15 18:44:55 +0200

    Control 1 - Pregunta 11

---
+++ control1/src/test/java/control1/control1/AppTest.java
@@ -0,0 +1,20 @@
+package control1.control1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
