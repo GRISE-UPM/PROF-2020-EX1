14dbd59c78251fde6cdcf24aaadf8fc75eec8029
Oscar Martinez Jimenez <oscar.martinez.jimenez@alumnos.upm.es>
2020-10-15 19:02:28 +0200

    subir proyecto a rama control-1

---
+++ control1/src/main/java/es/fi/upm/oscar/control1/App.java
@@ -0,0 +1,26 @@
+package es.fi.upm.oscar.control1;
+
+import org.apache.commons.io.IOUtils;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException
+    {
+        System.out.println( "Hello World!" );
+        
+        GitHub github = GitHub.connectAnonymously();
+
+        GHRepository repo = github.getRepository( "GRISE-UPM/PROF-2020-EX1" );
+
+        System.out.println( IOUtils.toString(repo.getReadme().read(), StandardCharsets.UTF_8) );
+    }
+}
