ba1ac0ab487ebabb4b71beffe2fd037a34b20fd1
Alvaro Recalde <elvarox301@gmail.com>
2021-10-21 17:36:51 +0200

    checkstyle

--- proyecto-control-1/src/main/java/proyecto/control/App.java
+++ proyecto-control-1/src/main/java/proyecto/control/App.java
@@ -1,13 +1,23 @@
 package proyecto.control;
 
+import org.apache.commons.io.IOUtils;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        GitHub github = GitHub.connectAnonymously();
+
+        GHRepository repo = github.getRepository( "GRISE-UPM/PROF-2020-EX1" );
+
+        System.out.println( IOUtils.toString(repo.getReadme().read(), StandardCharsets.UTF_8) );
     }
 }
